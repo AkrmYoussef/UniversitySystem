8dfa588062140013d195d01b4c055bcc814b0eb3
AkrmYoussef <dr.akrmyoussef410@gmail.com>
2024-05-20 17:30:19 +0300

    course controller modified

--- src/main/java/com/example/demo/service/CourseController.java
+++ src/main/java/com/example/demo/service/CourseController.java
@@ -3,16 +3,21 @@ package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.Course;
 
 import com.example.demo.repository.CourseRepository;
-
+import java.util.List;
 
 @RestController
 
 @RequestMapping("/api/courses")
 public class CourseController {
-     @Autowired
+    @Autowired
     private CourseRepository courseRepository;
    
+    @GetMapping("/getCourses")
+    public List<Course> getCourses() {
+        return courseRepository.findAll();
+    }
 
 }
